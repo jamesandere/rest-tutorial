6336e70f14fc4a9c37f20fb99f384e9f14cd10f7
jamesandere <jamesandere56@gmail.com>
2024-12-02 10:47:39 +0300

    first commit

---
+++ mytutorial/snippets/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
+from snippets import views
+
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet, basename='snippet')
+router.register(r'users', views.UserViewSet, basename='user')
+
+urlpatterns = [
+    path('', include(router.urls))
+]
